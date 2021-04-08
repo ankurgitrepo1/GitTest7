af0fd7d8ba67367134cad572a13ef4e8b31ff49f
ankurgitrepo1 <ankurg2007@gmail.com>
2021-04-08 20:51:06 +0530

    property4

--- ConsoleAppAp7/Feature1.cs
+++ ConsoleAppAp7/Feature1.cs
@@ -10,7 +10,9 @@ namespace ConsoleAppAp7
     {
         public int MyProperty1 { get; set; }
         public int MyProperty2 { get; set; }
-       
+
+        public int MyProperty4 { get; set; }
+
     }
 
 }
