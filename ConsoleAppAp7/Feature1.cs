6a8987fa1782fad2585efcbbe59ab4f4b6a5ab56
ankurgitrepo1 <81248096+ankurgitrepo1@users.noreply.github.com>
2021-04-08 20:50:11 +0530

    Update Feature1.cs
    updated

--- ConsoleAppAp7/Feature1.cs
+++ ConsoleAppAp7/Feature1.cs
@@ -10,6 +10,8 @@ namespace ConsoleAppAp7
     {
         public int MyProperty { get; set; }
         public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        
     }
 
 }
